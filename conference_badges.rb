80941b94a5e607ece6ceff74ad7224d4084aadc1
aurel fontaine <ajfame11@gmail.com>
2020-03-30 04:31:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
-def batch_badge_creator(Array)
+def batch_badge_creator(array)
   new_array = []
   array.each do |name|
     new_array << badge_maker(name)
@@ -14,6 +14,6 @@ def assign_rooms(attendees)
   return room_assignments
   end
   def printer(attendees)
-    puts batch_badge_creator(Array)
+    puts batch_badge_creator(array)
     puts assign_rooms(attendees)
 end
