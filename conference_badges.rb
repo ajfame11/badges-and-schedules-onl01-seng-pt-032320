a074cf490f72f6779fe8ed405fb1276d6d785b79
aurel fontaine <ajfame11@gmail.com>
2020-03-30 04:00:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array << badge_maker(name)
+  end
+  return new_array
+end
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index{ |name, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+  return room_assignments
+  end
